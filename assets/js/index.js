d0881154ce6cbed3d7c48297b12b392a864513f1
gonzalez-persevere <agonzalez@perseverenow.org>
2024-09-05 09:30:51 -0700

    I imported the json file and populated the page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,13 @@
 import data from '../../data.json' with {type: 'json'};
-
+const cardContainer = document.querySelector('.card-container');
+data.forEach(item => {
+    cardContainer.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src=${item.image.desktop} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${item.name}</h5>
+    <p class="card-text">${item.category}</p>
+    <p class="card-text">${item.price}</p>
+    <button class="btn btn-primary">Add to C</button>
+  </div>
+</div>`
+})
